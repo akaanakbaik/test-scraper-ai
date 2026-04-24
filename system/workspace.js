42300da05e52e97c50e1bb7d76355f6a713cfba0
aka <khaliqarrasyidabdul@gmail.com>
2026-04-24 18:21:25 +0700

    Update workspace.js

--- system/workspace.js
+++ system/workspace.js
@@ -1,21 +1,25 @@
 import fs from 'fs-extra'
 import path from 'path'
 
-export const paths = {
-  root: process.cwd(),
-  workspace: path.join(process.cwd(), 'workspace'),
-  input: path.join(process.cwd(), 'workspace', 'input'),
-  generated: path.join(process.cwd(), 'workspace', 'generated'),
-  logs: path.join(process.cwd(), 'workspace', 'logs'),
-  tmp: path.join(process.cwd(), 'workspace', 'tmp'),
-  reports: path.join(process.cwd(), 'reports')
-}
+export const rootDir = process.cwd()
+export const workspaceDir = path.join(rootDir, 'workspace')
+export const inputDir = path.join(workspaceDir, 'input')
+export const generatedDir = path.join(workspaceDir, 'generated')
+export const logsDir = path.join(workspaceDir, 'logs')
+export const tmpDir = path.join(workspaceDir, 'tmp')
+export const cacheDir = path.join(workspaceDir, 'cache')
+export const nodeCacheDir = path.join(cacheDir, 'node')
+export const pipCacheDir = path.join(cacheDir, 'pip')
+export const reportsDir = path.join(rootDir, 'reports')
 
 export const prepareWorkspace = async () => {
-  await fs.ensureDir(paths.workspace)
-  await fs.ensureDir(paths.input)
-  await fs.ensureDir(paths.generated)
-  await fs.ensureDir(paths.logs)
-  await fs.ensureDir(paths.tmp)
-  await fs.ensureDir(paths.reports)
+  await fs.ensureDir(workspaceDir)
+  await fs.ensureDir(inputDir)
+  await fs.ensureDir(generatedDir)
+  await fs.ensureDir(logsDir)
+  await fs.ensureDir(tmpDir)
+  await fs.ensureDir(cacheDir)
+  await fs.ensureDir(nodeCacheDir)
+  await fs.ensureDir(pipCacheDir)
+  await fs.ensureDir(reportsDir)
 }
