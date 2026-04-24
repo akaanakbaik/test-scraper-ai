d5cd619c41cc79d88f65b8b97a8a4dd2a1be4b86
aka <khaliqarrasyidabdul@gmail.com>
2026-04-24 21:18:17 +0700

    Update dependency.js

--- runtime/dependency.js
+++ runtime/dependency.js
@@ -2,7 +2,7 @@ import { execa } from 'execa'
 import ora from 'ora'
 import fs from 'fs-extra'
 import path from 'path'
-import { nodeCacheDir, pipCacheDir } from '../system/workspace.js'
+import { nodeCacheDir, pipCacheDir, generatedDir } from '../system/workspace.js'
 import { isDebug } from '../system/debug.js'
 
 const run = async cmd => {
@@ -25,6 +25,7 @@ const addFromError = (list, errorLog) => {
 
   const jsPatterns = [
     /Cannot find module ['"](.+?)['"]/g,
+    /Cannot find module ['"](.+?)['"] from/g,
     /Error \[ERR_MODULE_NOT_FOUND\].*?package ['"](.+?)['"]/g,
     /Cannot find package ['"](.+?)['"]/g
   ]
@@ -62,39 +63,79 @@ const uniqueDeps = deps => {
   return [...map.values()]
 }
 
-const ensureNodePackageJson = async () => {
-  const pkg = path.join(nodeCacheDir, 'package.json')
+const ensureNodePackageJson = async dir => {
+  const pkg = path.join(dir, 'package.json')
+
   if (await fs.pathExists(pkg)) return
 
   await fs.writeJson(pkg, {
-    type: 'module',
+    type: 'commonjs',
+    scripts: {
+      test: 'node test.js'
+    },
     dependencies: {}
   }, {
     spaces: 2
   })
 }
 
+const updateGeneratedPackage = async deps => {
+  const pkgPath = path.join(generatedDir, 'package.json')
+
+  let pkg = {}
+
+  if (await fs.pathExists(pkgPath)) {
+    pkg = await fs.readJson(pkgPath).catch(() => ({}))
+  }
+
+  pkg.scripts = pkg.scripts || {}
+  pkg.dependencies = pkg.dependencies || {}
+  pkg.devDependencies = pkg.devDependencies || {}
+
+  for (const dep of deps) {
+    const version = dep.version && dep.version !== 'latest' ? dep.version : 'latest'
+    if (!pkg.dependencies[dep.name] && !pkg.devDependencies[dep.name]) {
+      pkg.dependencies[dep.name] = version
+    }
+  }
+
+  await fs.writeJson(pkgPath, pkg, {
+    spaces: 2
+  })
+}
+
 const installNpmCached = async dep => {
-  await ensureNodePackageJson()
+  await ensureNodePackageJson(nodeCacheDir)
 
   const packageName = dep.version && dep.version !== 'latest' ? `${dep.name}@${dep.version}` : dep.name
-  const result = await execa('npm', ['install', packageName, '--prefix', nodeCacheDir], {
+
+  return await execa('npm', ['install', packageName, '--prefix', nodeCacheDir], {
     reject: false,
     stdio: isDebug() ? 'inherit' : 'pipe'
   })
+}
 
-  return result
+const installNpmGenerated = async dep => {
+  const packageName = dep.version && dep.version !== 'latest' ? `${dep.name}@${dep.version}` : dep.name
+
+  return await execa('npm', ['install', packageName, '--prefix', generatedDir], {
+    reject: false,
+    stdio: isDebug() ? 'inherit' : 'pipe'
+  })
 }
 
 const installPipCached = async dep => {
   const packageName = dep.version && dep.version !== 'latest' ? `${dep.name}==${dep.version}` : dep.name
 
-  const result = await execa('python3', ['-m', 'pip', 'install', packageName, '--target', pipCacheDir, '--break-system-packages'], {
+  return await execa('python3', ['-m', 'pip', 'install', packageName, '--target', pipCacheDir, '--break-system-packages'], {
     reject: false,
     stdio: isDebug() ? 'inherit' : 'pipe'
   })
+}
 
-  return result
+const checkNpmGenerated = async name => {
+  const target = path.join(generatedDir, 'node_modules', name)
+  return await fs.pathExists(target)
 }
 
 const checkNpmCached = async name => {
@@ -112,24 +153,45 @@ const checkPipCached = async name => {
 export const ensureDependencies = async (deps, lang, errorLog = '') => {
   const all = uniqueDeps(addFromError([...(deps || [])], errorLog))
 
+  if (!all.length) return []
+
+  if (lang === 'javascript') {
+    await updateGeneratedPackage(all)
+  }
+
   for (const dep of all) {
     const spinner = ora(`Memastikan dependency ${dep.name}`).start()
 
     if (lang === 'javascript') {
-      if (await checkNpmCached(dep.name)) {
-        spinner.succeed(`${dep.name} sudah ada di cache`)
+      const inGenerated = await checkNpmGenerated(dep.name)
+      const inCache = await checkNpmCached(dep.name)
+
+      if (inGenerated && inCache) {
+        spinner.succeed(`${dep.name} sudah ada`)
         continue
       }
 
-      const result = await installNpmCached(dep)
+      if (!inCache) {
+        const cacheResult = await installNpmCached(dep)
 
-      if (result.exitCode !== 0) {
-        spinner.fail(`Gagal install ${dep.name}`)
-        console.log(result.stderr || result.stdout || '')
-        process.exit(1)
+        if (cacheResult.exitCode !== 0) {
+          spinner.fail(`Gagal install cache ${dep.name}`)
+          console.log(cacheResult.stderr || cacheResult.stdout || '')
+          process.exit(1)
+        }
       }
 
-      spinner.succeed(`${dep.name} berhasil diinstall ke cache`)
+      if (!inGenerated) {
+        const generatedResult = await installNpmGenerated(dep)
+
+        if (generatedResult.exitCode !== 0) {
+          spinner.fail(`Gagal install sandbox ${dep.name}`)
+          console.log(generatedResult.stderr || generatedResult.stdout || '')
+          process.exit(1)
+        }
+      }
+
+      spinner.succeed(`${dep.name} siap`)
       continue
     }
 
@@ -150,4 +212,6 @@ export const ensureDependencies = async (deps, lang, errorLog = '') => {
       spinner.succeed(`${dep.name} berhasil diinstall ke cache`)
     }
   }
+
+  return all
 }
